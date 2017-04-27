3d8262d47df613d18bf75db0f450b5cb0e494a7b
Alejandro Dini <maildealete@gmail.com>
2017-04-27 18:48:14 -0300

    Add files via upload

---
+++ primo.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int n, noEsPrimo = 0;
+	
+	printf("Ingrese un entero positivo:\n");
+	fflush(stdin);
+	scanf("%d", &n);
+	
+	for(int i = 2; i <= (n / 2); ++i){
+		if(n%i == 0){
+			noEsPrimo = 1;
+			break;
+		}
+	}
+	if (noEsPrimo)
+		printf("%d no es primo",n);
+	else
+		printf("%d es primo",n);
+	getch();
+}
