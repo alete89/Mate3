d1d06307b9e27f6120990b91228270a67db97bb0
Alejandro Dini <maildealete@gmail.com>
2017-04-27 15:47:30 -0300

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int factorial(int n){
+	if(n == 0){
+		return 1;
+	}else{
+		return n*factorial(n-1);
+	}
+}
+
+int main(){
+	int n;
+	printf("Ingrese un valor:\n");
+	fflush(stdin);
+	scanf("%d",&n);
+	printf("El factorial de %d es: %d\n", n, factorial(n));
+	getch();
+}
---
+++ fibonacci.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int fibonacci(int n)
+{
+	if (n > 2)
+		return fibonacci(n-1) + fibonacci(n-2);
+	else if (n == 2)
+		return 1;
+	else if (n == 1)        
+		return 1;
+	else if (n == 0)
+		return 0;
+}
+
+int main(){
+	int valorIngresado;
+	
+	printf("Ingrese un valor máximo:\n");
+	fflush(stdin);
+	scanf("%d",&valorIngresado);
+	printf("0, 1");
+	for(int i=2; i < valorIngresado; i++){
+		printf(", %d",fibonacci(i));
+	}   
+	getch();
+}
